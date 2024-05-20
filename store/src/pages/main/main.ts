c622db3362816c7448c40793157a616faa2bb404
PavelKonstantinovich <89542944+PavelKonstantinovich@users.noreply.github.com>
2024-05-20 23:47:04 +0300

    Login page and some changes (#28)
    * feat: add login form and add prettier rules
    
    * feat: add favicon
    
    * feat: add password visibility
    
    * feat: add integrate login form with CommerceTools authorization service
    
    * feat: add error message
    
    * refactor: moving files for convenience
    
    * feat: add redirect and register button
    
    * fix: fix redirect bug
    
    * refactor: refactoring to make code more readable
    
    * fix: recovered accidentally deleted break
    
    * fix: fix adaptability errors

--- store/src/pages/main/main.ts
+++ store/src/pages/main/main.ts
@@ -9,18 +9,24 @@ import { locationResolver } from '../../components/event/locationResolver';
 class Main extends Component {
     constructor() {
         super('div', 'page');
-        const title = div('main');
-        title.changeText('Main Page (кнопки снизу для теста!)');
         this.appendChildren(
             new Header(),
             main(
                 'main',
                 div(
                     'container',
-                    title,
-                    new Button('header__button button', 'Product', { type: 'button' }, () =>
+                    new Button('main__button button', 'Product', { type: 'button' }, () =>
                         locationResolver('/product')
-                    )
+                    ),
+                    new Button('main__button button', 'Catalog', { type: 'button' }, () =>
+                        locationResolver('/catalog')
+                    ),
+                    new Button('main__button button', 'Cart', { type: 'button' }, () => locationResolver('/cart')),
+                    new Button('main__button button', 'About', { type: 'button' }, () => locationResolver('/about')),
+                    new Button('main__button button', 'Registration', { type: 'button' }, () =>
+                        locationResolver('/registration')
+                    ),
+                    new Button('main__button button', 'Login', { type: 'button' }, () => locationResolver('/login'))
                 )
             ),
             new Footer()
