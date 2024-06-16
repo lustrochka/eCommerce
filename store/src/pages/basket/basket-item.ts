d7f71e152e6217db0539632a1ff124788b6b9745
Olga <kartoshkaslukom@gmail.com>
2024-06-16 12:43:10 +0200

    feat: add changing itemquantity

---
+++ store/src/pages/basket/basket-item.ts
@@ -0,0 +1,50 @@
+import Component from '../../components/component/component';
+import { div, span, img, a } from '../../components/tags/tags';
+import Button from '../../components/button/button';
+import { LineItem } from '@commercetools/platform-sdk';
+import { changeItemQuantity } from '../../services/api/api';
+
+class BasketItem extends Component {
+    #quantityIndicator;
+
+    #price;
+
+    #priceIndicator;
+
+    #id;
+
+    constructor(data: LineItem) {
+        super('div', 'cart__item');
+        const src = data.variant.images ? data.variant.images[0].url : '';
+        let quantity = data.quantity;
+        this.#quantityIndicator = span('', data.quantity.toString());
+        this.#price = data.totalPrice.centAmount / 100;
+        this.#priceIndicator = span('cart__price', `${data.totalPrice.centAmount / 100}€` || '');
+        this.#id = data.id;
+
+        this.appendChildren(
+            img('cart__image', src, 'product photo'),
+            span('cart__name', data.name['en-GB']),
+            div(
+                'item-quantity',
+                new Button('', '-', {}, () => this.changeQuantity(--quantity)),
+                this.#quantityIndicator,
+                new Button('', '+', {}, () => this.changeQuantity(++quantity))
+            ),
+            this.#priceIndicator
+        );
+    }
+
+    changeQuantity(quantity: number) {
+        changeItemQuantity(this.#id, quantity).then(({ body }) => {
+            localStorage.setItem('cartVersion', body.version.toString());
+            if (quantity === 0) this.destroy();
+            else {
+                this.#quantityIndicator.changeText(quantity.toString());
+                this.#priceIndicator.changeText(`${quantity * this.#price}€`);
+            }
+        });
+    }
+}
+
+export default BasketItem;
